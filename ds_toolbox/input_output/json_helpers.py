5beb00e22f4b58c19378156d9ccf55bee67a50c7
michael-causalens <michael@causalens.com>
2022-07-15 09:04:51 +0100

    added numpy json decoder

--- ds_toolbox/input_output/json_helpers.py
+++ ds_toolbox/input_output/json_helpers.py
@@ -5,8 +5,36 @@ Helper functions for reading and writing to json and accessing data in nested py
 """
 
 import json
+import numpy as np
 from warnings import warn
 from collections import OrderedDict
+
+
+class NumpyEncoder(json.JSONEncoder):
+    """ Custom encoder for numpy data types """
+    def default(self, obj):
+        if isinstance(obj, (np.int_, np.intc, np.intp, np.int8,
+                            np.int16, np.int32, np.int64, np.uint8,
+                            np.uint16, np.uint32, np.uint64)):
+
+            return int(obj)
+
+        elif isinstance(obj, (np.float_, np.float16, np.float32, np.float64)):
+            return float(obj)
+
+        elif isinstance(obj, (np.complex_, np.complex64, np.complex128)):
+            return {'real': obj.real, 'imag': obj.imag}
+
+        elif isinstance(obj, (np.ndarray,)):
+            return obj.tolist()
+
+        elif isinstance(obj, np.bool_):
+            return bool(obj)
+
+        elif isinstance(obj, np.void):
+            return None
+
+        return json.JSONEncoder.default(self, obj)
 
 
 def write_json(out_dict, out_path, sort_keys=True):
